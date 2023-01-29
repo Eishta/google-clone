90e5e0fcc8aa85d08c44f6bca755f4ea2ef88fa1
Eishta <eishtamittal@gmail.com>
2023-01-29 15:58:18 +0530

    add mock data

---
+++ docs/APIKey.md
@@ -0,0 +1,21 @@
+### Get the API key
+
+1. Go to developers.google.com
+2. Scroll down and click on "View all products"
+3. Go to "ProgrammableSearch Engine" and explore
+4. Click on "Craete a Search Engine"
+5. In the left menu, choose "Using Rest" in the JSON API section
+6. Go to the example of GET API and copy the URL with all the paramaters
+7. Swap the API Key with your own API key
+8. Get the API key from the 1. point API key of the query parameters - Click the "identify your application"
+9. On the page opened, Click on "Get the Key" , it will open a popup
+10. Choose your app, here it is go-clone and click yes then Next
+11. Copy the key and paste in the env file on your local
+
+### GET the Context key
+
+1. From the second point of the params in URL, go the to "Control Panel"
+2. Click on "Create a New Search Engine"
+3. Choose the name of engine and give the sites to search(here we gave only www.google.com)
+4. Choose Image search and search entire web and Click on Create
+5. You will see a script code , copy the context from the code and paste in env file
--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Head from "next/head";
 import SearchHeader from "@/components/SearchHeader";
-
-export default function Search() {
+import Response from "../Response";
+export default function Search({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -13,4 +14,29 @@ export default function Search() {
       {/* Search Result  */}
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // let url = `https://www.googleapis.com/customsearch/v1?key=${
+  //   process.env.API_KEY
+  // }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+  //   context.query.searchType && "&searchType=image"
+  // }`;
+  // console.log(url);
+  let mock = true;
+
+  const data = mock
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+          context.query.searchType && "&searchType=image"
+        }`
+      ).then((response) => response.json());
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
